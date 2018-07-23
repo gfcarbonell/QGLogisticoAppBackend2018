41f10c26988a30b3c290734ef301c8fd233d83a1
Roberto Gian Franco Carbonell Saravia <r.gian.f.carbonell.s@gmail.com>
2018-07-22 23:28:35 -0500

    to modify person serializer

--- employees/serializers.py
+++ employees/serializers.py
@@ -1,20 +1,26 @@
 # -*- encoding: utf-8 -*-
 from rest_framework import serializers
 from .models import Employee
+from areas.models import Area
 from areas.serializers import AreaModelSerializer 
 from auth_users.serializers import AuthUserModelSerializer 
 from persons.serializers import PersonModelSerializer 
+from employee_types.models import EmployeeType
 from employee_types.serializers import EmployeeTypeModelSerializer
+from employee_positions.models import EmployeePosition
 from employee_positions.serializers import EmployeePositionModelSerializer
 
 # Serializers define the API representation.
 class EmployeeModelSerializer(serializers.ModelSerializer):
-    area = AreaModelSerializer()
+    area = AreaModelSerializer(read_only=True)
+    area_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Area.objects.all(), source='area')
     auth_user = AuthUserModelSerializer()
     person = PersonModelSerializer()
-    employee_position = EmployeePositionModelSerializer()
-    employee_type = EmployeeTypeModelSerializer()
-
+    employee_position = EmployeePositionModelSerializer(read_only=True)
+    employee_position_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=EmployeePosition.objects.all(), source='employee_position')
+    employee_type = EmployeeTypeModelSerializer(read_only=True)
+    employee_type_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=EmployeeType.objects.all(), source='employee_type')
+    
     def create(self, validated_data):
         person_data = validated_data.pop('person')
         person = PersonModelSerializer.create(PersonModelSerializer(), validated_data=person_data)
@@ -28,7 +34,7 @@ class EmployeeModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         fields = [
-            'id', 'area', 'employee_type', 'employee_position', 'person', 
+            'id', 'area', 'area_id', 'employee_type', 'employee_type_id', 'employee_position', 'employee_position_id', 'person', 
             'start_date_contract', 'end_date_contract', 
             'instruction_level', 'auth_user', 'active'
         ]
--- persons/serializers.py
+++ persons/serializers.py
@@ -1,14 +1,16 @@
 # -*- encoding: utf-8 -*-
 from rest_framework import serializers
 from .models import Person
+from contact_information.models import ContactInformation
 from contact_information.serializers import ContactInformationModelSerializer 
+from extra_information.models import ExtraInformation
 from extra_information.serializers import ExtraInformationModelSerializer
 
 
 # Serializers define the API representation.
 class PersonModelSerializer(serializers.ModelSerializer):
     contact_information = ContactInformationModelSerializer(many=True)
-    extra_information =  ExtraInformationModelSerializer(many=True)   
+    extra_information =  ExtraInformationModelSerializer(many=True) 
     class Meta:
         model = Person
         fields = [
@@ -16,4 +18,13 @@ class PersonModelSerializer(serializers.ModelSerializer):
                     'marital_status', 'photography', 'contact_information', 'extra_information'
                 ]
 
+    def create(self, validated_data):
+        contact_information_data = validated_data.pop('contact_information')
+        contact_information = ContactInformationModelSerializer.create(ContactInformationModelSerializer(), validated_data=contact_information)
+
+        extra_information_data = validated_data.pop('extra_information')
+        extra_information = ExtraInformationModelSerializer.create(ExtraInformationModelSerializer(), validated_data=extra_information_data)
+
+        person, created = Person.objects.update_or_create(contact_information=contact_information, extra_information=extra_information, **validated_data)
+        return person
 
